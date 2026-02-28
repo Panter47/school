d2d1a24dad0b6ba1e937bda14bc4f4056015c108
Aaron <tchenaaron@gmail.com>
2026-02-28 23:58:49 +0100

    macchina del caffè ad oggetti

--- Informatica/python/Macchina del caffè/main.py
+++ Informatica/python/Macchina del caffè/main.py
@@ -1,3 +1,34 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+inFunzione=True
+menu = Menu()
+saldo = MoneyMachine()
+macchinaCaffè = CoffeeMaker() 
+
+bevanda = None
+
+while(inFunzione):
+    print()
+    print(menu.get_items())
+    scelta = input("scegliere tra le bevande sopra elencate quale si desidera: ")
+    bevanda = menu.find_drink(scelta.lower())
+    if bevanda == None:
+        if scelta.lower() == "off":
+            print("macchina del caffè spenta")
+            break
+        if scelta.lower() == "report":
+            print()
+            macchinaCaffè.report()
+            saldo.report()
+    else:
+        if macchinaCaffè.is_resource_sufficient(bevanda):
+            print()
+            if saldo.make_payment(bevanda.cost):
+                macchinaCaffè.make_coffee(bevanda)
+
+
+        
+    
